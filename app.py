a60c2319fb8eb0635f8f4e478c58b73050a7a94f
Fernando Ruaro <landell.ruaro@gmail.com>
2017-11-06 23:02:26 -0200

    Adding parameter

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ import urllib2
 
 PORT = int(os.getenv('PORT', 8080))
 SERVICE = os.getenv('SERVICE', None)
+PARAMETER = os.getenv('PARAMETER', None)
 
 class my_handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
    def do_GET(self):
       self.send_response(200)
       self.send_header('Content-type','text/html')
       self.end_headers()
-      result  = "<b>HOSTNAME:</b> " + os.environ.get('HOSTNAME')
+      result  = "<b>HOSTNAME:</b> " + os.environ.get('HOSTNAME') + " - " + PARAMETER
       if SERVICE is not None:
         result += "<br/><b>Consumindo servico:</b> "+SERVICE+"<br/>" + urllib2.urlopen(SERVICE).read()
       self.wfile.write(result)
